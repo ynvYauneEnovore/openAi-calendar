ff1a813705536c2dc4904269fc843b0634313c6b
ynvYauneEnovore <yovanuxf@gmail.com>
2023-04-29 23:22:26 -0400

    m,m

--- resources/js/Components/Crear.vue
+++ resources/js/Components/Crear.vue
@@ -1,110 +1,44 @@
 <template #main>
-
-<div class="row row-cols-1 row-cols-md-3 g-4">
-  <div class="col">
-    <div class="card h-100">
-      <img src="/img/card.png" class="card-img-top" alt="/img/card.png">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+<div class="container w-40">
+<div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/img/card.png" class="d-block w-50 h-40" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="/img/card-1.png" class="d-block w-50 h-40" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="/img/card-2.png" class="d-block w-50" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card h-100">
-      <img src="/img/card.png" class="card-img-top" alt="/img/card.png">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a short card.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card h-100">
-      <img src="/img/card.png" class="card-img-top" alt="/img/card.png">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card h-100">
-      <img src="/img/card.png" class="card-img-top" alt="/img/card.png">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-    </div>
-  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
-
-    <div class="row">
-    <div class="col-sm-6">
-        <div class="card bg-dark text-white">
-        <div class="card-body">
-            <h5 class="card-title">Creación de nuevas tareas</h5>
-            <p class="card-text">Permite a los usuarios agregar nuevas tareas con sus respectivos detalles, como descripción, fecha de inicio, fecha de vencimiento, prioridad.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-        </div>
-    </div>
-    <div class="col-sm-6">
-        <div class="card bg-dark text-white">
-        <div class="card-body">
-            <h5 class="card-title">Creación de nuevas tareas</h5>
-            <p class="card-text">Permite a los usuarios agregar nuevas tareas con sus respectivos detalles, como descripción, fecha de inicio, fecha de vencimiento, prioridad.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-        </div>
-    </div>
-    <div class="col-sm-6">
-        <div class="card bg-dark text-white">
-        <div class="card-body">
-            <h5 class="card-title">Listado de tareas</h5>
-            <p class="card-text">Muestra todas las tareas disponibles en el sistema en una tabla o lista, ordenadas por fecha de vencimiento o por prioridad, según corresponda. Permite a los usuarios ver el estado de cada tarea y si ha sido completada o no.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-        </div>
-    </div>
-    <div class="col-sm-6">
-        <div class="card bg-dark text-white">
-        <div class="card-body">
-            <h5 class="card-title">Edición de tareas</h5>
-            <p class="card-text">Permite a los usuarios actualizar los detalles de una tarea existente, como la descripción, la fecha de inicio, la fecha de vencimiento, la prioridad.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-        </div>
-    </div>
-    <div class="col-sm-6">
-        <div class="card bg-dark text-white">
-        <div class="card-body">
-            <h5 class="card-title">Eliminación de tareas</h5>
-            <p class="card-text">Permite a los usuarios eliminar tareas que ya no sean necesarias o que hayan sido completadas.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-        </div>
-    </div>
-    </div>
-
-
-        <h5>
-            Filtrado de tareas
-        </h5>
-        <p>
-            Ofrece opciones de filtrado para que los usuarios puedan ver solo las tareas que les interesen, por ejemplo, aquellas que estén asignadas a ellos o aquellas que tengan una fecha de vencimiento próxima.
-        </p>
-        <h5>
-            Asignación de tareas
-        </h5>
-        <p>
-            Permite a los usuarios asignar tareas a otros usuarios, si el sistema cuenta con roles y permisos que lo permitan.
-        </p>
-        <h5>
-            Comentarios y notas
-        </h5>
-        <p>
-            Ofrece la posibilidad de que los usuarios agreguen comentarios o notas a las tareas, para mantener un registro de su progreso o cualquier otra información relevante.</p>
+</div>
   </template>
 
   <script>
